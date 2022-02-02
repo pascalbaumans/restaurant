98059a0b1f69aa72478bc17fc59252fbfc95be03
pascalbaumans <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>
2022-02-02 20:51:00 +0100

    add key

--- src/main/java/entity/Plat.java
+++ src/main/java/entity/Plat.java
@@ -4,12 +4,18 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.math.BigDecimal;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table
 public class Plat {
+    @Id
+    @SequenceGenerator(name = "plat_sequence",sequenceName = "plat_sequence",allocationSize = 1)
+    @GeneratedValue(generator = "plat_sequence", strategy = GenerationType.SEQUENCE)
     private Long id;
     private BigDecimal price;
     private String name;
